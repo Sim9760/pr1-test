1e9e0e9b30032b6930a735fa4f8ca9ad66d2827b
Simon Liétar <simon@lietar.net>
2022-08-04 14:30:09 +0200

    Update the electron forge configuration

--- app/electron/forge.config.js
+++ app/electron/forge.config.js
@@ -12,6 +12,10 @@ module.exports = {
     { name: '@electron-forge/maker-dmg' },
     { name: '@electron-forge/maker-zip',
       platforms: ['darwin', 'linux'] },
-    { name: '@electron-forge/maker-squirrel' }
+    { name: '@electron-forge/maker-squirrel',
+      config: {
+        authors: 'LBNC',
+        description: 'Protocol Runner 1'
+      } }
   ]
 }
